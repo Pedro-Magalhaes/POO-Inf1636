c7fc1127255a7bc1364a576f562aad940f965f0f
Pedro Magalhaes <pfsmagalhaes@gmail.com>
2018-03-13 22:36:28 -0300

    Colocando getters e setters

--- Lab01/Socio.java
+++ Lab01/Socio.java
@@ -11,10 +11,53 @@ public class Socio {
     public Socio(int x) {
         matricula = x;
     }
-    
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    // Setters
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    /**
+     * @param dtNasc the dtNasc to set
+     */
+    public void setDtNasc(String dtNasc) {
+        this.dtNasc = dtNasc;
+    }
+
+    // Getters
+    /**
+     * @return the matricula
+     */
     public String getMatric() {
         String digito = Integer.toString(matricula % 10);
         return Integer.toString(matricula / 10) + "-" + digito;
     }
+    /**
+     * @return the dtNasc
+     */
+    public String getDtNasc() {
+        return dtNasc;
+    }
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+    
     
 }
